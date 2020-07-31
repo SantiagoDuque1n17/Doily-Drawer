e99b34808e924995025d747f9ed8a50105c11239
Santiago Duque <34222605+SantiagoDuque1n17@users.noreply.github.com>
2020-07-31 14:47:08 +0200

    Add files via upload

---
+++ Gallery.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.awt.*;
+import javax.swing.*;
+
+public class Gallery extends JPanel {
+
+    /**
+	 * 
+	 */
+	//private static final long serialVersionUID = 1L;
+	ArrayList<JLabel> gallery;
+
+    public ArrayList<JLabel> getGallery() {
+		return gallery;
+	}
+    
+    public void delGallery(int index) {
+		gallery.remove(index);
+		remove(index);
+	}
+    
+    public void addGallery(JLabel image){
+    	gallery.add(image);
+    	add(image);
+    }
+    
+    
+
+	public Gallery()
+    {
+        super();
+        gallery = new ArrayList<JLabel>();
+        setLayout(new GridLayout(4, 3));
+        setVisible(true);
+        
+        
+        
+    }
+	  
+
+
+}
---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+
+	public static void main(String[] args) {
+		MainWindow letsRunIt = new MainWindow(900, 1600, "Digital Doily");
+	}
+}
